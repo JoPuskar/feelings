5ac0bce6a39a470f15944ec1266570fbc9557827
JoPuskar <puskarjoshi22@gmail.com>
2017-11-16 21:21:26 +0545

    Login Logout Sorted

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import url, include
+from django.views.generic import TemplateView
+
+from . import views
+from .views import LogoutView
 
 urlpatterns = [
-    url('^', include('django.contrib.auth.urls'))
+    url(r'^dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
+    url('^', include('django.contrib.auth.urls')),
 ]

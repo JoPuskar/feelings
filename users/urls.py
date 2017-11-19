1dc4655eaff93819cd9c024fd62ac4c5df2ab0a4
JoPuskar <puskarjoshi22@gmail.com>
2017-11-19 12:45:48 +0545

    Dashboard requires login

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from . import views
 from .views import LogoutView
 
 urlpatterns = [
-    url(r'^dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^dashboard/$', views.Dashboard.as_view(), name='dashboard'),
     url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
     url(r'^signup/$', views.SignUpView.as_view(), name='signup'),
     url('^', include('django.contrib.auth.urls')),

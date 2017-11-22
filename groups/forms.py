dc4fe2893a5ce88423eafd3c112661fe5809c5b1
JoPuskar <puskarjoshi22@gmail.com>
2017-11-23 01:03:00 +0545

    Invites can now be created, can't be accepted.

--- groups/forms.py
+++ groups/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.db.models import Q
 
 from . import models
 
@@ -13,3 +16,18 @@ class CompanyForm(forms.ModelForm):
     class Meta:
         fields = ('name', 'description')
         model = models.Company
+
+
+class CompanyInviteForm(forms.Form):
+    email_or_username = forms.CharField(label="Email or Username")
+
+    def clean_email_or_username(self):
+        data = self.cleaned_data['email_or_username']
+        try:
+            self.invitee = models.User.objects.get(
+                Q(email=data)|Q(username=data)
+            )
+        except models.User.DoesNotExist:
+            raise ValidationError('No such user')
+
+        return data

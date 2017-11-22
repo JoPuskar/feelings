45cee7906dd7efde5185fad4fa8075b9c1186a64
JoPuskar <puskarjoshi22@gmail.com>
2017-11-22 15:32:34 +0545

    Companies split out successfully, first three views

--- groups/urls.py
+++ groups/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import url, include
 
-from .views import CompanyCreateView
+from .views import company
+from .views import family
+
+
+company_patterns = [
+    url(r'^create/$', company.Create.as_view(), name='create'),
+    url(r'^edit/(?P<slug>[-\w]+)/$', company.Update.as_view(), name='update'),
+    url(r'^(?P<slug>[-\w]+)/$', company.Detail.as_view(), name='detail'),
+]
 
 urlpatterns = [
-    url(r'^company/create/$', CompanyCreateView.as_view(), name='create_company')
+    url(r'^companies/', include(company_patterns, namespace='companies'))
 ]
---
+++ groups/views/company.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse_lazy
+from django.views import generic
+
+from braces.views import SetHeadlineMixin
+
+from django import forms
+from ..import forms
+
+
+class Create(LoginRequiredMixin, SetHeadlineMixin, generic.CreateView):
+    form_class = forms.CompanyForm
+    headline = 'Create Company'
+    template_name = 'companies/form.html'
+    success_url = reverse_lazy('users:dashboard')
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        response = super().form_valid(form)
+        self.object.members.add(self.request.user)
+        return response
+
+
+class Update(LoginRequiredMixin, SetHeadlineMixin, generic.UpdateView):
+    form_class = forms.CompanyForm
+    template_name = 'companies/form.html'
+    success_url = reverse_lazy('users:dashboard')
+
+    def get_queryset(self):
+        return self.request.user.companies.all()
+
+    def get_headline(self):
+        return 'Edit {}'.format(self.object.name)
+
+
+class Detail(LoginRequiredMixin, SetHeadlineMixin, generic.DetailView):
+    template_name = 'companies/detail.html'
+
+    def get_queryset(self):
+        return self.request.user.companies.all()
+
+    def get_headline(self):
+        return 'Edit {}'.format(self.object.name)

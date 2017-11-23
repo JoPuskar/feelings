09994bc604d7893f2dfd790be06f962d251d0a1a
JoPuskar <puskarjoshi22@gmail.com>
2017-11-23 16:53:20 +0545

    Status Choices

--- feelings/settings.py
+++ feelings/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'g1@+37s(eduk5*jp49aqf3)2)^*c7$nrls9unp_#a!kc($8h&s'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+INTERNAL_IPS = ['127.0.0.1']
 
 # Application definition
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'bootstrap3',
     'users',
     'groups',
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',

7618a31490a7f671abb7840de7484ec3cd5ffb98
JoPuskar <puskarjoshi22@gmail.com>
2017-11-27 12:32:36 +0545

    JWT Auth

--- feelings/urls.py
+++ feelings/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
+from rest_framework_jwt.views import obtain_jwt_token
 
 from thoughts import urls as thoughts_urls
 from users import urls as user_urls
@@ -39,6 +40,7 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
     url(r'^api/', include(api_urlpatterns)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', obtain_jwt_token),
 ]
 
 if settings.DEBUG:

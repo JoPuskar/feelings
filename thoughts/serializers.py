9a8c743b54eb98615c91e848aeaf622645a3f7ab
JoPuskar <puskarjoshi22@gmail.com>
2017-11-27 22:22:47 +0545

    Relation URLs and condition display field.

--- thoughts/serializers.py
+++ thoughts/serializers.py
@@ -4,9 +4,11 @@ from rest_framework import routers, serializers, viewsets
 
 
 class ThoughtSerializer(serializers.HyperlinkedModelSerializer):
+    condition_display = serializers.SerializerMethodField()
+
     class Meta:
         model = models.Thought
-        fields = ('recorded_at', 'condition', 'notes')
+        fields = ('recorded_at', 'condition', 'condition_display', 'notes', 'user')
         read_only_fields = ('recorded_at',)
 
     def create(self, validated_data):
@@ -14,3 +16,6 @@ class ThoughtSerializer(serializers.HyperlinkedModelSerializer):
         thought.user = self.context['request'].user
         thought.save()
         return thought
+
+    def get_condition_display(self, obj):
+        return obj.get_condition_display()
